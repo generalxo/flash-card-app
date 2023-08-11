fac8a605bff541afdc184db19efa5f1b15878f8e
Oliver Wasner <oli.wasner@hotmail.com>
2023-08-11 08:11:12 +0200

    cards and views

---
+++ flash-card-app/Models/FlashCardModel.cs
@@ -0,0 +1,10 @@
+﻿namespace flash_card_app.Models
+{
+    internal class FlashCardModel
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public int Streak { get; set; } = 0;
+
+    }
+}
