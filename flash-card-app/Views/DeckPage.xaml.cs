0058e00b60b55e7525ef46147e77bb88f46601d3
Oliver Wasner <oli.wasner@hotmail.com>
2023-09-12 10:57:55 +0200

    renaming

--- flash-card-app/Views/DeckPage.xaml.cs
+++ flash-card-app/Views/DeckPage.xaml.cs
@@ -2,18 +2,18 @@ using flash_card_app.ViewModels;
 
 namespace flash_card_app.Views;
 
-public partial class CardDeckPage : ContentPage
+public partial class DeckPage : ContentPage
 {
-    private readonly DeckViewModel _viewModel;
-    public CardDeckPage(DeckViewModel viewModel)
-    {
-        InitializeComponent();
-        BindingContext = viewModel;
-        _viewModel = viewModel;
-    }
+	private readonly DeckViewModel _viewModel;
+	public DeckPage(DeckViewModel viewModel)
+	{
+		InitializeComponent();
+		BindingContext = viewModel;
+		_viewModel = viewModel;
+	}
 
-    protected override void OnAppearing()
-    {
-        _viewModel.GetCardDecksCommand.Execute(null);
-    }
+	protected override void OnAppearing()
+	{
+		_viewModel.GetCardDecksCommand.Execute(null);
+	}
 }
